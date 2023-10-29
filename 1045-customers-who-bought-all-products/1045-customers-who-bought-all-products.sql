c44001bc88bc937d262310194b84111603b97579
Yinuo Zhao <94703030+inorrr@users.noreply.github.com>
2023-10-29 01:22:58 -0400

    Time: 827 ms (96.64%), Space: 0B (100.00%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,27 +1,18 @@
 # Write your MySQL query statement below
 
-# CREATE VIEW id as
 # SELECT DISTINCT customer_id
-# FROM Customer;
+# FROM customer
+# WHERE customer_id not in(
+#     SELECT DISTINCT c.customer_id
+#     FROM Customer c
+#     CROSS JOIN Product p
+#     LEFT JOIN Customer pc ON c.customer_id = pc.customer_id 
+#     AND p.product_key = pc.product_key
+#     WHERE pc.customer_id IS NULL
+# );
 
-# CREATE VIEW expected AS
-# SELECT *
-# FROM id cross join Product;
-
-# CREATE VIEW not_all AS
-# (expected) - (Customer);
-
-# CREATE VIEW all_product as 
-# (expected) - (not all);
-
-# select distinct customer_id from all_product;
-
-SELECT DISTINCT customer_id
-from customer
-where customer_id not in(
-SELECT DISTINCT c.customer_id
-FROM Customer c
-CROSS JOIN Product p
-LEFT JOIN Customer pc ON c.customer_id = pc.customer_id 
-AND p.product_key = pc.product_key
-WHERE pc.customer_id IS NULL);
+SELECT customer_id
+ FROM customer c
+  GROUP BY customer_id
+   HAVING count(distinct product_key)=(SELECT count(distinct product_key) 
+    FROM product)
