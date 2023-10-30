a9586cb8c4ab10a0f1c94cc1b667824ad6b0c7d9
Yinuo Zhao <94703030+inorrr@users.noreply.github.com>
2023-10-30 01:09:02 -0400

    Time: 993 ms (75.17%), Space: 0B (100.00%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,4 +1,11 @@
-# Write your MySQL query statement below
-SELECT employee_id, department_id
-FROM Employee w
-WHERE primary_flag = 'Y' OR (SELECT count(*) FROM Employee e1 WHERE e1.employee_id = w.employee_id) = 1;
+# # Write your MySQL query statement below
+# SELECT employee_id, department_id
+# FROM Employee w
+# WHERE primary_flag = 'Y' OR (SELECT count(*) FROM Employee e1 WHERE e1.employee_id = w.employee_id) = 1;
+
+SELECT employee_id,department_id from Employee
+group by employee_id
+having count(employee_id)=1 
+UNION 
+SELECT employee_id,department_id from Employee
+where primary_flag='Y'
