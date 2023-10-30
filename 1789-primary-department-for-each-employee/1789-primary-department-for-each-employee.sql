62f5e48ef4c5a4d025aee9db4d26900ff37b5200
Yinuo Zhao <94703030+inorrr@users.noreply.github.com>
2023-10-30 01:09:50 -0400

    Time: 1256 ms (28.67%), Space: 0B (100.00%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -3,9 +3,9 @@
 # FROM Employee w
 # WHERE primary_flag = 'Y' OR (SELECT count(*) FROM Employee e1 WHERE e1.employee_id = w.employee_id) = 1;
 
-SELECT employee_id,department_id from Employee
+(SELECT employee_id,department_id from Employee
 group by employee_id
-having count(employee_id)=1 
+having count(employee_id)=1 )
 UNION 
-SELECT employee_id,department_id from Employee
-WHERE primary_flag='Y';
+(SELECT employee_id,department_id from Employee
+WHERE primary_flag='Y');
