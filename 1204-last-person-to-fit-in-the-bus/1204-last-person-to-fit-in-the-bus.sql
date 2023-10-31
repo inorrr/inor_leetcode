3e79c42b45f76355b7362b2134d8506eff5d6566
Yinuo Zhao <94703030+inorrr@users.noreply.github.com>
2023-10-31 00:17:49 -0400

    Time: 3590 ms (7.95%), Space: 0B (100.00%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-SELECT person_name FROM
-(SELECT * FROM Queue ORDER BY turn) AS ordered
-WHERE 1000 >= (SELECT sum(weight) FROM queue WHERE turn <= ordered.turn)
+SELECT person_name
+FROM Queue q1
+WHERE 1000 >= (SELECT sum(weight) FROM Queue WHERE turn <= q1.turn)
 ORDER BY turn DESC
 LIMIT 1;
